fadc20bae43c63948bac30cb4b6f3062e7389f02
Matthieu Lepaix <matthieu.lepaix@gmail.com>
2020-06-28 16:11:25 -0400

    Updates

---
+++ precache-manifest.7254c538fbe9ba700bde68c9848428c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78377c2bda81cf2d7d64ec5dabf67fa2",
+    "url": "/lescabinesnordikdespiles/index.html"
+  },
+  {
+    "revision": "d4a8f3d03a425abfe4dc",
+    "url": "/lescabinesnordikdespiles/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "634340a76f63b0808db7",
+    "url": "/lescabinesnordikdespiles/static/js/2.c2c22a61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lescabinesnordikdespiles/static/js/2.c2c22a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a8f3d03a425abfe4dc",
+    "url": "/lescabinesnordikdespiles/static/js/main.fc6da0a0.chunk.js"
+  },
+  {
+    "revision": "81760d3d4fb8a84b51bd",
+    "url": "/lescabinesnordikdespiles/static/js/runtime-main.85f569dd.js"
+  }
+]);
